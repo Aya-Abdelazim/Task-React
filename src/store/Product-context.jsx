27be8a850beb9864002de4ea855893704f5b71b8
Aya-Abdelazim <143610851+Aya-Abdelazim@users.noreply.github.com>
2024-07-11 14:19:00 +0300

    x Product-context.jsx

--- src/store/Product-context.jsx
+++ src/store/Product-context.jsx
@@ -1,45 +1,45 @@
-import { createContext, useState } from "react";
+// import { createContext, useState } from "react";
 
-const ProductContext = createContext({
-  data: [],
-  totalBasket: 0,
-  addToBasket: () => {},
-  removeFromBasket: () => {},
-  itemInBasket: () => {},
-});
+// const ProductContext = createContext({
+//   data: [],
+//   totalBasket: 0,
+//   addToBasket: () => {},
+//   removeFromBasket: () => {},
+//   itemInBasket: () => {},
+// });
 
-export function ProductContextProvider(props) {
-  const [data, setData] = useState([]);
+// export function ProductContextProvider(props) {
+//   const [data, setData] = useState([]);
 
-  function addToBasketHandler(product) {
-    setData((prevData) => {
-      return [...prevData, product];
-    });
-  }
+//   function addToBasketHandler(product) {
+//     setData((prevData) => {
+//       return [...prevData, product];
+//     });
+//   }
 
-  function removeFromBasketHandler(productId) {
-    setData((prevData) => {
-      return prevData.filter((item) => item.id !== productId);
-    });
-  }
+//   function removeFromBasketHandler(productId) {
+//     setData((prevData) => {
+//       return prevData.filter((item) => item.id !== productId);
+//     });
+//   }
 
-  function itemInBasketHandler(productId) {
-    return data.some((item) => item.id === productId);
-  }
+//   function itemInBasketHandler(productId) {
+//     return data.some((item) => item.id === productId);
+//   }
 
-  const context = {
-    data: data,
-    totalBasket: data.length,
-    addToBasket: addToBasketHandler,
-    removeFromBasket: removeFromBasketHandler,
-    itemInBasket: itemInBasketHandler,
-  };
+//   const context = {
+//     data: data,
+//     totalBasket: data.length,
+//     addToBasket: addToBasketHandler,
+//     removeFromBasket: removeFromBasketHandler,
+//     itemInBasket: itemInBasketHandler,
+//   };
 
-  return (
-    <ProductContext.Provider value={context}>
-      {props.children}
-    </ProductContext.Provider>
-  );
-}
+//   return (
+//     <ProductContext.Provider value={context}>
+//       {props.children}
+//     </ProductContext.Provider>
+//   );
+// }
 
-export default ProductContext;
+// export default ProductContext;
